a1726c2c6cbf4b2a6f56304e2945944fa9d4d565
ywwwtseng <ywwwtseng@gmail.com>
2019-06-05 15:59:34 +0800

    add multi view example

---
+++ lib/templates/default/src/components/pages/Muitl/SimpleView.vue
@@ -0,0 +1,8 @@
+<template lang="html">
+  <div>
+    SimpleVeiw
+    <button @click="$emit('changeView', 'ComplexView')">
+      changeView
+    </button>
+  </div>
+</template>
---
+++ lib/templates/default/src/pages/multi.vue
@@ -0,0 +1,34 @@
+<script>
+import SimpleView from '@/components/pages/Muitl/SimpleView';
+import ComplexView from '@/components/pages/Muitl/ComplexView';
+
+export default {
+  data() {
+    return {
+      view: 'SimpleView'
+    }
+  },
+
+  methods: {
+    changeView(view) {
+      this.view = view;
+    }
+  },
+
+  render() {
+    const props = {
+      props: {
+        // data
+      },
+      on: {
+        // methods
+        changeView: this.changeView
+      },
+    };
+
+    if (this.view === 'SimpleView') return <SimpleView {...props} />;
+    if (this.view === 'ComplexView') return <ComplexView {...props} />;
+    return <div>404</div>;
+  }
+}
+</script>
